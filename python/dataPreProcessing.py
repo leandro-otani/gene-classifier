e9a908c6bed1aabf7eaa13c645af1f9ac2cb2205
Leandro Otani <lotani@Leandros-MacBook-Pro.local>
2017-08-12 03:31:45 -0300

    T-value in progress

--- python/dataPreProcessing.py
+++ python/dataPreProcessing.py
@@ -2,6 +2,9 @@ import csv
 import collections
 import arff
 import itertools
+import numpy as np
+import pandas as pd
+import scipy.stats as st
 
 class DataPreProcessing:
     #extracao da planilha de dados
@@ -34,19 +37,51 @@ class DataPreProcessing:
                 if tempVal < minVal:
                     minVal = tempVal
 
-            # if (maxVal - minVal)/2 >= 2:
-            if (maxVal - minVal) > 200 and (maxVal/minVal) > 2:
+            if (maxVal - minVal)/2 >= 2:
+            # if (maxVal - minVal) > 200 and (maxVal/minVal) > 2:
                 cleansedSet.append(column)
 
-        print len(cleansedSet)
+        # print len(cleansedSet)
 
-        columnSet = zip(cleansedSet)[1:]
-        for column in columnSet:
-            label = classes[int(column[0])-1]
-            mutableTuple = [label]
-            mutableTuple += [float(value) for value in list(column)[1:]]
-            cleanseData()
-            pp5.append(mutableTuple)
+        classes = self.retrieveClasses("pp5i_train_class.txt")
+
+        cleansedSet.append(classes)
+
+        # print cleansedSet
+
+
+        df = pd.DataFrame(cleansedSet[1:])
+        df.to_csv('./clean-dataset.csv', index=False, header=False)
+
+        a = itertools.izip(*csv.reader(open("./clean-dataset.csv", "rb")))
+        csv.writer(open("./train-transposed.csv", "wb")).writerows(a)
+
+        dadosLimpos = pd.read_csv('./train-transposed.csv');
+        genes = dadosLimpos.columns[:-1]
+        classes = pd.Series(dadosLimpos['class'].values.ravel()).unique()
+
+
+        genesTvalues = {}
+        for classe in classes:
+            tgenes = []
+            for gene in genes:
+                sample1 = dadosLimpos[dadosLimpos['class'] == classe][gene]
+                sample2 = dadosLimpos[dadosLimpos['class'] != classe][gene]
+                t = st.ttest_ind(sample1,sample2,equal_var=False)
+                tgenes.append(t[0])
+            genesTvalues[classe] = pd.Series(tgenes,index=genes)
+        genesTvaluesDf = pd.DataFrame(genesTvalues)
+
+        print genesTvalues
+
+        # columnSet = zip(cleansedSet)[1:]
+        # for column in columnSet:
+        #     label = classes[int(column[0])-1]
+        #     mutableTuple = [label]
+        #     mutableTuple += [float(value) for value in list(column)[1:]]
+        #     cleanseData()
+        #     pp5.append(mutableTuple)
+
         return pp5
 
     #separa rotulos dos dados e devolve lista contendo (rotulo, conteudo)
@@ -57,10 +92,11 @@ class DataPreProcessing:
 
     def retrieveClasses(self, inputFileWithClasses):
         with open(inputFileWithClasses, 'rb') as f_in:
+            header = ["class"]
             content = f_in.read()
             content = str(content).replace("\r", " ").replace('\n', '\n') #removendo caracteres inuteis
             content = content.split('\n') #separa linhas
-            return content[1:len(content) - 1]
+            return (header + content[1:len(content) - 1])
 
 
 if __name__ == "__main__":
